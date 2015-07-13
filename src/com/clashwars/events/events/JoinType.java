5240f6295bf361b1ac045dcc111083afc0b2cc6f
Jos <josroossien@hotmail.com>
2015-07-13 04:23:31 +0200

    +Added two new JoinType's IN_GAME for when a player tries to join while he's already in the game and QUEUE for when the player joins before the 10 second timer has started. When joining in QUEUE the player doesn't get teleported and has to wait for more players and when it's JOIN the player will be teleported to the game directly since the 10s timer has already started.

--- src/com/clashwars/events/events/JoinType.java
+++ src/com/clashwars/events/events/JoinType.java
@@ -9,6 +9,8 @@ public enum JoinType {
     FULL, /** When the session is full */
     ENDED, /** When the session has ended */
     RESETTING, /** When the session is still resetting */
+    IN_GAME, /** When the player is already in a session. (Most likely queued but doesn't have to be) */
+    QUEUE, /** When the player can join the session but the 10 second timer hasn't started */
     JOIN, /** When the player can join the session */
     JOIN_BACK, /** When the player joins back while a session is on hold */
     SPECTATE; /** When the session is started but the player can spectate */
