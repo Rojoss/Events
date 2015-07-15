1a695c7e4670e9985337020ff078b07e93a67187
Jos <josroossien@hotmail.com>
2015-07-15 15:30:07 +0200

    +Added lots of listeners to KOH to allow damaging players, shooting bows, throwing potions. +Added respawn listener to respawn player back in the map with new equipment (still need to decrease lives on death and put player in spectator when dead)

--- src/com/clashwars/events/events/koh/Koh.java
+++ src/com/clashwars/events/events/koh/Koh.java
@@ -2,15 +2,31 @@ package com.clashwars.events.events.koh;
 
 import com.clashwars.cwcore.debug.Debug;
 import com.clashwars.cwcore.helpers.CWItem;
+import com.clashwars.cwcore.utils.CWUtil;
+import com.clashwars.events.events.EventType;
 import com.clashwars.events.events.GameSession;
+import com.clashwars.events.events.State;
 import com.clashwars.events.modifiers.IntModifierOption;
 import com.clashwars.events.modifiers.Modifier;
 import com.clashwars.events.modifiers.ModifierOption;
+import com.clashwars.events.player.CWPlayer;
 import com.clashwars.events.setup.SetupOption;
 import com.clashwars.events.setup.SetupType;
 import com.clashwars.events.events.BaseEvent;
+import com.clashwars.events.util.Util;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.potion.PotionType;
 
 import java.util.ArrayList;
@@ -46,6 +62,69 @@ public class Koh extends BaseEvent {
         equipment.add(new CWItem(PotionType.INSTANT_DAMAGE, true, modifierOptions.get(Modifier.KOH_DAMAGE_POTIONS).getInteger()));
         equipment.add(new CWItem(Material.ARROW, 1).setSlot(9));
         return equipment;
+    }
+
+
+    @EventHandler
+    private void death(PlayerDeathEvent event) {
+        //TODO: Decrease lives.
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    private void respawn(PlayerRespawnEvent event) {
+        CWPlayer cwp = getCWPlayer(event.getPlayer());
+
+        GameSession session = cwp.getSession();
+        if (session == null || session.getType() != EventType.KOH) {
+            return;
+        }
+
+        //TODO: Put player in spectator if out of lives.
+
+        CWUtil.resetPlayer(event.getPlayer(), GameMode.SURVIVAL);
+        Util.equipItems(event.getPlayer(), getEquipment(session));
+
+        Location loc = session.getTeleportLocation(cwp);
+        Debug.bc(loc);
+        event.setRespawnLocation(loc);
+    }
+
+
+    @EventHandler
+    private void entityDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player)) {
+            return;
+        }
+
+        if (event.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK && event.getCause() != EntityDamageEvent.DamageCause.PROJECTILE &&
+                event.getCause() != EntityDamageEvent.DamageCause.FALL && event.getCause() != EntityDamageEvent.DamageCause.MAGIC) {
+            return;
+        }
+
+        if (validateSession((Player)event.getEntity(), EventType.KOH, false, State.STARTED)) {
+            event.setCancelled(false);
+        }
+    }
+
+    @EventHandler
+    private void projectileShoot(ProjectileLaunchEvent event) {
+        if (!(event.getEntity().getShooter() instanceof Player)) {
+            return;
+        }
+        if (validateSession((Player)event.getEntity().getShooter(), EventType.KOH, false, State.STARTED)) {
+            event.setCancelled(false);
+        }
+    }
+
+    @EventHandler
+    private void interact(PlayerInteractEvent event) {
+        if (event.getItem() == null || event.getItem().getType() != Material.POTION) {
+            return;
+        }
+        if (validateSession(event.getPlayer(), EventType.KOH, false, State.STARTED)) {
+            event.setUseItemInHand(Event.Result.ALLOW);
+            event.setCancelled(false);
+        }
     }
 
 }
