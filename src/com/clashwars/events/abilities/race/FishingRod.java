f1d2af9abb17379633c36cff1be8ddd864bbf4c0
Jos <josroossien@hotmail.com>
2015-07-30 00:28:45 +0200

    %One time usage to race fishingrod powerup.

--- src/com/clashwars/events/abilities/race/FishingRod.java
+++ src/com/clashwars/events/abilities/race/FishingRod.java
@@ -2,12 +2,15 @@ package com.clashwars.events.abilities.race;
 
 import com.clashwars.cwcore.debug.Debug;
 import com.clashwars.cwcore.helpers.CWItem;
+import com.clashwars.cwcore.utils.CWUtil;
 import com.clashwars.events.abilities.Ability;
 import com.clashwars.events.abilities.BaseAbility;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 
@@ -28,10 +31,17 @@ public class FishingRod extends BaseAbility {
             return;
         }
         if (event.getCaught() instanceof Player) {
-            Player p = event.getPlayer();
+            final Player p = event.getPlayer();
             Vector dir = p.getLocation().getDirection();
             dir = dir.multiply(-1);
             event.getCaught().setVelocity(new Vector(dir.getX(), 0.5, dir.getZ()));
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    p.getInventory().setItem(0, new ItemStack(Material.AIR));
+                    p.updateInventory();
+                }
+            }.runTaskLater(events, 1);
         }
     }
 
