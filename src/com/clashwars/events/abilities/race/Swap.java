b22d868a601d95d594e34a1e18e932cebc4987db
Jos <josroossien@hotmail.com>
2015-07-30 00:37:19 +0200

    +Added swap ability for race.

--- src/com/clashwars/events/abilities/race/Swap.java
+++ src/com/clashwars/events/abilities/race/Swap.java
@@ -1,11 +1,14 @@
 package com.clashwars.events.abilities.race;
 
+import com.clashwars.cwcore.events.DelayedPlayerInteractEvent;
 import com.clashwars.cwcore.helpers.CWItem;
 import com.clashwars.cwcore.packet.ParticleEffect;
 import com.clashwars.cwcore.utils.CWUtil;
 import com.clashwars.events.abilities.Ability;
 import com.clashwars.events.abilities.BaseAbility;
 import com.clashwars.events.damage.AbilityDmg;
+import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -24,5 +27,32 @@ public class Swap extends BaseAbility {
         castItem = new CWItem(Material.EYE_OF_ENDER, 1, (short)0, displayName);
     }
 
+    @Override
+    public void castAbility(final Player player, Location triggerloc) {
+
+        final Player target = CWUtil.getTargetedPlayer(player, 10);
+
+        if (target == null) {
+            CWUtil.sendActionBar(player, CWUtil.integrateColor("&4&l>> &cLook at a player within 10 blocks range and click to use! &4&l<<"));
+            return;
+        }
+
+        Location targetLoc = target.getLocation();
+        Location playerLoc = player.getLocation();
+
+        target.teleport(playerLoc);
+        player.teleport(targetLoc);
+        player.getWorld().playEffect(playerLoc, Effect.ENDER_SIGNAL, 1);
+        player.getWorld().playEffect(targetLoc, Effect.ENDER_SIGNAL, 1);
+        player.getWorld().playSound(playerLoc, Sound.ENDERMAN_TELEPORT, 0.5f, 0.6f);
+        player.getWorld().playSound(targetLoc, Sound.ENDERMAN_TELEPORT, 1, 1.2f);
+    }
+
+
+
+    @EventHandler
+    public void interact(DelayedPlayerInteractEvent event) {
+        super.interact(event);
+    }
 
 }
